743b0a6488cd8d7a5b88bd094520cae54282bcf8
noelfoka <nomgnenoel@gmail.com>
2023-11-10 14:07:20 +0100

    added images

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -10,9 +10,15 @@ const AuthLayout = () => {
         <Navigate to="/" />
       ) : (
         <>
-          <section>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
+
+          <img 
+            src="/assets/images/back.jpg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
         </>
       )
     }
