fe21d9a93c9051a7a11b906167b3496ec2e3915d
noelfoka <nomgnenoel@gmail.com>
2023-11-10 12:45:55 +0100

    organize all the principals routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { Routes, Route } from 'react-router-dom'
 
 import './globals.css'
+import AuthLayout from './_auth/AuthLayout'
+import SigninForm from './_auth/forms/SigninForm'
+import SignupForm from './_auth/forms/SignupForm'
+import RootLayout from './_root/RootLayout'
+import Home from './_root/pages/Home'
 
 const App = () => {
   return (
     <main className='flex h-screen'>
         <Routes>
             {/* Public route */}
-            <Route path='/sign_in' element={<SigninForm />} />
+            <Route element={<AuthLayout />}>
+                <Route path='/sign_in' element={<SigninForm />} />
+                <Route path='/sign_up' element={<SignupForm />} />
+            </Route>
 
             {/* Private route */}
-            <Route index element={<Home />} />
+            <Route element={<RootLayout />}>
+                <Route index element={<Home />} />
+            </Route>
 
         </Routes>
     </main>
