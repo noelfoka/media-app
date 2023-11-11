ebe5817ab48e66eb2b94c18d46b44867b16e38df
noelfoka <nomgnenoel@gmail.com>
2023-11-11 04:50:54 +0100

    start setup appwrite

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -2,9 +2,14 @@ import { Client, Account, Databases, Storage, Avatars } from 'appwrite'
 
 export const appwriteConfig = {
     projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    url: import.meta.env.VITE_APPWRITE_URL,
 }
 
 export const client = new Client();
+
+client.setProject(appwriteConfig.projectId);
+client.setEndpoint(appwriteConfig.url)
+
 export const account = new Account(client);
 export const database = new Databases(client);
 export const storage = new Storage(client);
