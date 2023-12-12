e6f4452e8cc0736c2375f26330fbc30abd8fabe0
noelfoka <nomgnenoel@gmail.com>
2023-12-12 11:20:02 +0100

    start home page starting to architecture

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,17 @@
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import Topbar from '@/components/shared/Topbar'
 import React from 'react'
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className='w-full md:flex'>
+      <Topbar />
+      <LeftSidebar />
+
+      <section className='flex flex-1'>
+
+      </section>
+    </div>
   )
 }
 
